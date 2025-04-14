dbeb99afc3693da65dc637045a4b2da50a602e50
kolani florent <kolaniflorent446@gmail.com>
2025-04-14 15:55:18 +0000

    commit

--- FRONT/front_end/src/services/api.js
+++ FRONT/front_end/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/API';
+const API_URL = 'http://192.168.1.70:5000/API';
 
 export const fetchTickets = () => axios.get(`${API_URL}/tickets`);
 export const fetchTicketById = (id) => axios.get(`${API_URL}/tickets/${id}`);
